70e76456a02d993a399fc23fbef2170d3fe39052
ivan0961 <incivan_09@hotmail.com>
2016-08-29 01:12:01 +0300

    First commit

---
+++ Mesajlasma/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Mesajlasma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.labell = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lbContacts = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.detailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtSent = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtName.Location = new System.Drawing.Point(228, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(166, 23);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMail.Location = new System.Drawing.Point(228, 52);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(166, 23);
+            this.txtMail.TabIndex = 1;
+            // 
+            // txtNick
+            // 
+            this.txtNick.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNick.Location = new System.Drawing.Point(228, 78);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(166, 23);
+            this.txtNick.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(160, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label.Location = new System.Drawing.Point(160, 58);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(37, 17);
+            this.label.TabIndex = 1;
+            this.label.Text = "Mail:";
+            // 
+            // labell
+            // 
+            this.labell.AutoSize = true;
+            this.labell.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labell.Location = new System.Drawing.Point(160, 84);
+            this.labell.Name = "labell";
+            this.labell.Size = new System.Drawing.Size(39, 17);
+            this.labell.TabIndex = 1;
+            this.labell.Text = "Nick:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.Location = new System.Drawing.Point(446, 25);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(129, 76);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "ADD CONTACT";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSend.Location = new System.Drawing.Point(12, 158);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(129, 29);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send Message";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 205);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(155, 196);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // lbContacts
+            // 
+            this.lbContacts.ContextMenuStrip = this.contextMenuStrip1;
+            this.lbContacts.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbContacts.FormattingEnabled = true;
+            this.lbContacts.ItemHeight = 16;
+            this.lbContacts.Location = new System.Drawing.Point(197, 205);
+            this.lbContacts.Name = "lbContacts";
+            this.lbContacts.Size = new System.Drawing.Size(177, 196);
+            this.lbContacts.TabIndex = 5;
+            this.lbContacts.SelectedIndexChanged += new System.EventHandler(this.lbContacts_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.detailsToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(110, 48);
+            // 
+            // detailsToolStripMenuItem
+            // 
+            this.detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
+            this.detailsToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.detailsToolStripMenuItem.Text = "Details";
+            this.detailsToolStripMenuItem.Click += new System.EventHandler(this.detailsToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // txtSent
+            // 
+            this.txtSent.Location = new System.Drawing.Point(400, 205);
+            this.txtSent.Name = "txtSent";
+            this.txtSent.Size = new System.Drawing.Size(175, 196);
+            this.txtSent.TabIndex = 7;
+            this.txtSent.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 413);
+            this.Controls.Add(this.txtSent);
+            this.Controls.Add(this.lbContacts);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.labell);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label labell;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.ListBox lbContacts;
+        private System.Windows.Forms.RichTextBox txtSent;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem detailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
